bd28709649acfb027dd8de4cf0bd7c07711ff539
zachpeterw <zwhi8516@740s-pl-129.local>
2016-10-03 12:01:50 -0600

    linked the UpdatedViewController.swift file to the associated ViewController in the storyboard

---
+++ Controller/UpdatedViewController.swift
@@ -0,0 +1,14 @@
+//
+//  UpdatedViewController.swift
+//  FirstTransitions
+//
+//  Created by Whitehead, Zachary on 10/3/16.
+//  Copyright © 2016 Whitehead, Zachary. All rights reserved.
+//
+
+import UIKit
+
+class UpdatedViewController : UIViewController
+{
+    
+}
