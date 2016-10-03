9c93a7c29ffdf8cf398bd42aac8a91555b3363db
zachpeterw <zwhi8516@740s-pl-129.local>
2016-10-03 11:41:45 -0600

    created project

---
+++ FirstTransitions/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirstTransitions
+//
+//  Created by Whitehead, Zachary on 10/3/16.
+//  Copyright © 2016 Whitehead, Zachary. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
